bb1f03a285938264257c143473fc4c7751655313
Eivind Liland <spookysys@gmail.com>
2017-02-16 00:27:36 +0100

    Looping over all files

--- encode.m
+++ encode.m
@@ -1,114 +1,100 @@
 block_size = 8;
 adpcm_bits = 2;
 
-[data_in, sample_rate] = load_input('snare-808', block_size);
-audiowrite('orig.wav', data_in, sample_rate);
-
-[bass_orig, bass_env_values, bass_cut_point, treble_orig, treble_env_fit, treble_b, treble_a] = separate(data_in, sample_rate, block_size);
-audiowrite('bass_orig.wav', bass_orig, round(sample_rate/8));
-audiowrite('treble_orig.wav', treble_orig, round(sample_rate/8));
-
-bass_orig = bass_orig(1:bass_cut_point);
-bass_env_values = bass_env_values(1:bass_cut_point);
-audiowrite('bass_cut.wav', bass_orig, round(sample_rate/8));
-
-bass_4bit = digitize_bass(bass_orig);
-audiowrite('bass_4bit.wav', bass_4bit, round(sample_rate/8));
-
-treble_recon = recon_treble(length(data_in), treble_env_fit, treble_b, treble_a, sample_rate);
-audiowrite('treble_recon.wav', treble_recon, sample_rate);
-
-bass_env_identity = ones(length(bass_env_values), 1);
-bass_norm = bass_orig ./ bass_env_values;
-% bass_norm = min(10, max(-10, bass_norm));
-[bass_norm_adpcm, bass_norm_palette] = compress_adpcm(bass_norm, bass_env_identity, adpcm_bits);
-bass_norm_recon = decompress_adpcm(bass_norm_adpcm, bass_norm_palette);
-bass_recon = bass_norm_recon .* bass_env_values;
-audiowrite('bass_recon.wav', bass_recon, round(sample_rate/8));
-
-[bass_flat_adpcm, bass_flat_palette] = compress_adpcm(bass_orig, bass_env_identity, adpcm_bits);
-bass_flat_recon = decompress_adpcm(bass_flat_adpcm, bass_flat_palette);
-audiowrite('bass_flat_recon.wav', bass_flat_recon, round(sample_rate/8));
+files = dir('in/*.wav');
+for file = files'
+    filename = file.name;
+    disp(['Processing ' filename]);
+    main(['in/' filename], ['out/' filename], ['out_check/' filename '/'], block_size, adpcm_bits);
+end
 
 
-% mix and output
-padding = zeros(length(data_in)/block_size - length(bass_orig), 1);
-bass_orig_padded = [bass_orig; padding];
-bass_recon_padded = [bass_recon; padding];
-bass_flat_recon_padded = [bass_flat_recon; padding];
-bass_upsampled = resample(bass_orig_padded, block_size, 1);
-bass_recon_upsampled = resample(bass_recon_padded, block_size, 1);
-bass_flat_recon_upsampled = resample(bass_flat_recon_padded, block_size, 1);
-mix00 = bass_upsampled + treble_orig;
-mix01 = bass_upsampled + treble_recon;
-mix10 = bass_recon_upsampled + treble_orig;
-mix11 = bass_recon_upsampled + treble_recon;
-mix20 = bass_flat_recon_upsampled + treble_orig;
-mix21 = bass_flat_recon_upsampled + treble_recon;
-audiowrite('mix00.wav', mix00, sample_rate);
-audiowrite('mix01.wav', mix01, sample_rate);
-audiowrite('mix10.wav', mix10, sample_rate);
-audiowrite('mix11.wav', mix11, sample_rate);
-audiowrite('mix20.wav', mix20, sample_rate);
-audiowrite('mix21.wav', mix21, sample_rate);
+function main(infile, outfile, checkdir, block_size, adpcm_bits)
+    mkdir(checkdir);
+    [data_in, sample_rate] = load_input(infile, block_size);
+
+    audiowrite([checkdir 'orig.wav'], data_in, sample_rate);
+
+    [bass, bass_env_values, treble, treble_env_fit, treble_b, treble_a] = separate(data_in, sample_rate, block_size);
+
+    audiowrite([checkdir 'bass.wav'], bass, round(sample_rate/block_size));
+    audiowrite([checkdir 'treble.wav'], treble, sample_rate);
+        
+    treble_recon = reconstruct_treble(length(data_in), treble_env_fit, treble_b, treble_a, sample_rate);
+    audiowrite([checkdir 'treble_recon.wav'], treble_recon, sample_rate);
+
+    bass_env_identity = ones(length(bass_env_values), 1);
+    bass_norm = bass ./ bass_env_values;
+    [bass_norm_adpcm, bass_norm_palette] = compress_adpcm(bass_norm, bass_env_identity, adpcm_bits);
+    bass_norm_recon = decompress_adpcm(bass_norm_adpcm, bass_norm_palette);
+    bass_recon = bass_norm_recon .* bass_env_values;
+    audiowrite([checkdir 'bass_recon.wav'], bass_recon, round(sample_rate/block_size));
+
+    % mix and output
+    padding = zeros(length(data_in)/block_size - length(bass), 1);
+    bass_recon_padded = [bass_recon; padding];
+    bass_recon_upsampled = resample(bass_recon_padded, block_size, 1);
+    mix = bass_recon_upsampled + treble_recon;
+    audiowrite(outfile, mix, sample_rate);
+    audiowrite([checkdir 'result.wav'], mix, sample_rate);
+end
 
 
 % Load wav-file
-function [data, sample_rate] = load_input(stub, block_size)
-	filename = ['in/'  stub  '.wav'];
+function [data, sample_rate] = load_input(filename, block_size)
 	[data, sample_rate] = audioread(filename);
     padding = ceil(length(data) / block_size) * block_size - length(data); % pad
-    data = [data zeros(padding, 1)]; % pad
+    pad = zeros(padding, 1);
+    data = [data; pad]; % pad
     scaler = max(abs(data)); % normalize
     data = data / scaler; % normalize
 end
 
 % Prepare data for compression
-function [bass_orig, bass_env_values, bass_cut_point, treble_orig, treble_env_fit, treble_b, treble_a] = separate(data_in, sample_rate, block_size)
+function [bass, bass_env_values, treble, treble_env_fit, treble_b, treble_a] = separate(data_in, sample_rate, block_size)
 	nyquist = sample_rate/2.0;
 	separation_freq = nyquist/block_size/2.0;
 
-    % separate and resample bass
-    bass_orig = resample(data_in, 1, block_size);
-    bass_orig = normalize(bass_orig);
-    
-	% separate treble
-	[sep_treble_b, sep_treble_a] = butter(6, separation_freq / nyquist, 'high');
-	treble_orig = filter(sep_treble_b, sep_treble_a, data_in);
-	    
-	% find volume envelopes
-	bass_env = abs(hilbert(bass_orig));
-	treble_env = abs(hilbert(treble_orig));
+    % extract and resample bass
+    bass = resample(data_in, 1, block_size);
+    bass = normalize(bass);
 
-    % fit treble volume envelope
-    treble_env_fit_x = (1:length(data_in))';
-    treble_env_fit = fit(treble_env_fit_x, treble_env, 'exp2');
-    
     % fit bass volume envelope
-    bass_env_fit_x = (1:length(bass_orig))';
+    bass_env = abs(hilbert(bass));
+    bass_env_fit_x = (1:length(bass))';
     bass_env_fit = fit(bass_env_fit_x, bass_env, 'exp2');
     bass_env_values = feval(bass_env_fit, bass_env_fit_x);
     
     % cut bass at the point where volume drops below 1/256
-    % but also make sure envelope never goes below that point
-    disp(max(bass_env_values));
     bass_cut_point = 0;
-    limit = 1. / 2^10;
-    for i = 1:length(bass_orig)
+    limit = 1. / 2^8;
+    for i = 1:length(bass)
         if (bass_env_values(i) > limit)
             bass_cut_point = i;
         end
     end
+    bass = bass(1:bass_cut_point);
+    bass_env_values = bass_env_values(1:bass_cut_point);
     
+    
+	% separate treble
+	[sep_treble_b, sep_treble_a] = butter(6, separation_freq / nyquist, 'high');
+	treble = filter(sep_treble_b, sep_treble_a, data_in);
+	    
+	% find volume envelopes
+	treble_env = abs(hilbert(treble));
+    treble_env_fit_x = (1:length(data_in))';
+    treble_env_fit = fit(treble_env_fit_x, treble_env, 'exp2');
+
     % extract treble color
     num_freqs = 1024;
-    treble_h = fft(treble_orig, num_freqs);
+    treble_h = fft(treble, num_freqs);
     treble_h = treble_h(1:num_freqs/2+1);
     treble_w = (0:(num_freqs/2)) * (2*pi)/num_freqs;
     [treble_b, treble_a] = invfreqz(treble_h, treble_w, 2, 2);
     treble_ab_scaler = max(abs([treble_a(2:end) treble_b]));
     treble_a = treble_a / treble_ab_scaler;
-    treble_b = treble_b / treble_ab_scaler;
+    treble_b = treble_b / treble_ab_scaler;    
 end
 
 
@@ -207,27 +193,8 @@ function data_out = decompress_adpcm(data_in, palette)
     end
 end
 
-% bass is in [-1:1]
-function [res, scaler] = digitize_bass(val)
-    bits = 4;
 
-    % normalize
-    scaler = max(abs(val));
-    res = val / scaler;
-    
-    % digitize
-    limit = 2 ^ bits;
-    res = (res + 1.) * ((limit-1) / 2.);
-    res = res + rand(size(res));
-    res = round(res);
-    res = min(limit-1, max(0.0, res));
-    
-    % for convenience
-    res = ((res ./ (limit-1)) - 0.5) * 2;
-end
-
-
-function data_out = recon_treble(num_samples, env_fit, treble_b, treble_a, sample_rate)
+function data_out = reconstruct_treble(num_samples, env_fit, treble_b, treble_a, sample_rate)
     white = rand(num_samples, 1) * 2 - 1;
 
     colored = filter(treble_b, treble_a, white);
@@ -237,6 +204,7 @@ function data_out = recon_treble(num_samples, env_fit, treble_b, treble_a, sampl
     env_values = feval(env_fit, env_fit_x); 
     data_out = colored .* env_values;
 end
+
 
 function [data_out, scaler] = normalize(data_in) 
     scaler = max(abs(data_in));
