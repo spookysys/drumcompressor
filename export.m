7f3ed0e863ed9a4a7249c4df18670d99c6328387
Eivind Liland <spookysys@gmail.com>
2017-02-25 00:17:18 +0100

    minor debugging stuff in export.m

--- export.m
+++ export.m
@@ -1,5 +1,7 @@
 function [ output_args ] = export(wav_filename, bass_data, bass_palette, bass_env_fit, treble_b, treble_a, treble_env_fit)
-    [data_fid, struct_fid, name] = start(wav_filename);
+    [log_fid, data_fid, struct_fid, name] = start(wav_filename);
+
+    fprintf(log_fid, [name '\n']);
     
     % output bass_data
     bass_data_reshaped = reshape(bass_data, [4, length(bass_data)/4]);
@@ -16,10 +18,22 @@ function [ output_args ] = export(wav_filename, bass_data, bass_palette, bass_en
     
     % output struct
     bass_env_coeff = coeffvalues(bass_env_fit);
+    if length(bass_env_coeff) ~= 4
+        bass_env_coeff = [0 0 0 0];
+    end
     treble_env_coeff = coeffvalues(treble_env_fit);
+    if length(treble_env_coeff) ~= 4
+        treble_env_coeff = [0 0 0 0];
+    end
+    
     
     % a and c values should be scaled to -128:+127 to give max volume
     ac = [bass_env_coeff(1), bass_env_coeff(3), treble_env_coeff(1), treble_env_coeff(3)];
+    acsum = sum(ac);
+    fprintf(log_fid, ['Sum of ac values: ' num2str(acsum) '\n']);
+    fprintf(log_fid, ['bass_env_fit: ' num2str(coeffvalues(bass_env_fit)) '\n']);
+    fprintf(log_fid, ['treble_env_fit: ' num2str(coeffvalues(treble_env_fit)) '\n']);
+    
     boost_candidates = [127.49/max(ac) -128.49/min(ac)];
     boost = min(boost_candidates(boost_candidates > 0));
     ac = ac * boost;
@@ -28,12 +42,15 @@ function [ output_args ] = export(wav_filename, bass_data, bass_palette, bass_en
     bass_env_coeff(3) = ac(2);
     treble_env_coeff(1) = ac(3);
     treble_env_coeff(3) = ac(4);
+   % fprintf(struct_fid, 
     
+    fprintf(log_fid, '\n');
+    fclose(log_fid);
     fclose(data_fid);
     fclose(struct_fid);
 end
 
-function [data_fid, struct_fid, name] = start(wav_filename)
+function [log_fid, data_fid, struct_fid, name] = start(wav_filename)
      [~,~,~] = mkdir(fullfile('export', 'data'));
      [~,~,~] = mkdir(fullfile('export', 'struct'));
      [~, name, ~] = fileparts(wav_filename);
@@ -65,7 +82,8 @@ function [data_fid, struct_fid, name] = start(wav_filename)
      fprintf(fid, [ '#endif\n']);
      fclose(fid);
      clear fid;
-     
+          
+     log_fid = fopen(fullfile('export', 'log.txt'), 'a');
      data_fid = fopen(fullfile('export', 'data', [name '.inc']), 'w');
      struct_fid = fopen(fullfile('export', 'struct', [name '.inc']), 'w');
 end
